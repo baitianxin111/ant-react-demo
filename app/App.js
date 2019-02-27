7087647bafbc2eefcfd2da0cd3b73e13f5479b5c
baitianxin111 <495017135@qq.com>
2019-02-27 17:49:46 +0800

    app

---
+++ app/App.js
@@ -0,0 +1,18 @@
+import React, { Component } from 'react';
+import Home from '../app/components/Home'
+import ReactDOM from 'react-dom';
+import './css/test.css';
+class App extends Component{
+    constructor(props){
+        super(props);
+    }
+    render() {
+        return(
+        <div>
+            <Home></Home>
+           <h1 className="sss">hello, world，就想测试实时a!</h1>,
+        </div>
+        );
+    }
+}
+module.exports = App;
--- app/index.js
+++ app/index.js
@@ -1,10 +1,12 @@
 import React from "react";
 import ReactDom from "react-dom";
-import '../src/css/index.sass'
-import '../src/css/test.css'
+import App from './App.js'
+import '../app/css/index.sass';
+import '../app/css/test.css';
 
 ReactDom.render(
-    <h1 className="sss">hello, world，就想测试实时a!</h1>,
+    <App />,
+    // <h1 className="sss">hello, world，就想测试实时a!</h1>,
     // <image src="../src/images/logo.jpg"></image>,
     document.getElementById("root")
 );
